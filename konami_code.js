23213cdd2e22f3c1cffd09024050101648ce8f9e
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-17 13:47:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,21 @@ const codes = [
 function init() {
   // your code here
   const body = document.querySelector('body');
-  
+  let index = 0;
+  function onKeyDownHandler(e){
+    const key = e.key;
+    if(key === codes[index]){
+      index++;
+      
+      if(index === codes.length){
+        alert('Congratulations!!')
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
   body.addEventListener(codes[0], fonction(e){
     if(e.key === codes[0]){
      alert('Congratulations') 
